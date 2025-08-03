a4372d0106d4de8281cf62d6653ea27478f64071
njodib <naomibasterash@gmail.com>
2025-08-03 18:57:21 -0400

    19

--- 18.py
+++ 18.py
@@ -6,17 +6,14 @@ s = b64decode(b'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSv
 key = b'YELLOW SUBMARINE'
 nonce = 0
 
-def ks():
-    ct = 0
-    cipher = AES.new(key, AES.MODE_ECB)
-    while True:
-        pee = pack("QQ", nonce, ct)
-        ct += 1
-        yield from cipher.encrypt(pee)
-
 def dec():
+    def ks():
+        ct = 0
+        cipher = AES.new(key, AES.MODE_ECB)
+        while True:
+            pee = pack("QQ", nonce, ct) #packs as little-endian 64-bit
+            ct += 1
+            yield from cipher.encrypt(pee)
     return bytes((xb^yb) for xb,yb in zip(bytearray(s), ks()))
-
-enc = dec
 
 print(dec().decode())
---
+++ 19.py
@@ -0,0 +1,87 @@
+# This is the manual-decryption tool described in NCC
+# Manual is fine for this!
+
+from Crypto.Cipher import AES
+from struct import pack
+from base64 import b64decode
+from random import randbytes
+from Utils.BytesLogic import xor
+
+txts = [
+    b'SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==',
+    b'Q29taW5nIHdpdGggdml2aWQgZmFjZXM=',
+    b'RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==',
+    b'RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=',
+    b'SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk',
+    b'T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
+    b'T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=',
+    b'UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
+    b'QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=',
+    b'T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl',
+    b'VG8gcGxlYXNlIGEgY29tcGFuaW9u',
+    b'QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==',
+    b'QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=',
+    b'QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==',
+    b'QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=',
+    b'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=',
+    b'VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==',
+    b'SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==',
+    b'SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==',
+    b'VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==',
+    b'V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==',
+    b'V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==',
+    b'U2hlIHJvZGUgdG8gaGFycmllcnM/',
+    b'VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=',
+    b'QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=',
+    b'VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=',
+    b'V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=',
+    b'SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==',
+    b'U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==',
+    b'U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=',
+    b'VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==',
+    b'QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu',
+    b'SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=',
+    b'VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs',
+    b'WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=',
+    b'SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0',
+    b'SW4gdGhlIGNhc3VhbCBjb21lZHk7',
+    b'SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=',
+    b'VHJhbnNmb3JtZWQgdXR0ZXJseTo=',
+    b'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4='
+]
+
+key = randbytes(16)
+nonce = 0
+
+def enc(msg):
+    def ks():
+        ct = 0
+        cipher = AES.new(key, AES.MODE_ECB)
+        while True:
+            pee = pack("QQ", nonce, ct) #packs as little-endian 64-bit
+            ct += 1
+            yield from cipher.encrypt(pee)
+    return bytes((xb^yb) for xb,yb in zip(bytearray(msg), ks()))
+
+ctxts = [enc(b64decode(c)) for c in txts]
+
+def keystream_from_input(ind, guess):
+    p_i = guess.encode('utf-8')
+    c_i = ctxts[ind]
+    ks = xor(p_i, c_i)
+
+    for j,c_j in enumerate(ctxts):
+        p_j = xor(c_j, ks)
+        p_j += b'?'*(len(c_j)-len(ks))
+        print(j, p_j.decode())
+
+if __name__ == '__main__':
+    ind, guess = 0,''
+    while True:
+        print()
+        keystream_from_input(ind,guess)
+        print()
+
+        try:new_ind, new_guess = input("> ").split(" ", 1)
+        except:continue
+        else: ind = int(new_ind)
