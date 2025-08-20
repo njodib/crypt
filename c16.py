8c0d18b3e543860755c855da622eed34bd662c3c
njodib <naomibasterash@gmail.com>
2025-08-20 19:33:04 -0400

    27

--- c16.py
+++ c16.py
@@ -10,14 +10,13 @@ class Oracle:
         self.post = b";comment2=%20like%20a%20pound%20of%20bacon"
         self.cipher = AES_CBC(self.key, self.iv)
 
-    def encrypt(self, user_input: bytes):
-        
-        ptxt = str(user_input).replace(';', '').replace('=', '').encode()
+    def encode(self, user_input: bytes):
+        ptxt = user_input.replace(b';', b'').replace(b'=', b'')
         ptxt = self.pre + ptxt + self.post
         ctxt = self.cipher.enc(ptxt, pad=True)
         return ctxt
 
-    def decrypt_and_check_admin(self, ctxt):
+    def parse(self, ctxt):
         data = self.cipher.dec(ctxt, strip=True)
         return b';admin=true' in data
 
@@ -25,11 +24,11 @@ if __name__ == "__main__":
     # how come i need comments to explain everything?
     # like ummm just look at it!!
     profile = Oracle()
-    ctxt = profile.encrypt("hello9admin9true")
+    ctxt = profile.encode(b"hello9admin9true")
     block2 = ctxt[16:32]
     bit_flipper = b'\x00'*5+b'\x02'+b'\x00'*5+b'\x04'+b'\x00'*4
     flipped_block2 = xor(block2, bit_flipper)
     ctxt_flipped = ctxt[:16] + flipped_block2 + ctxt[32:]
 
-    assert profile.decrypt_and_check_admin(ctxt_flipped)
+    assert profile.parse(ctxt_flipped)
     print("SUCCESS")
